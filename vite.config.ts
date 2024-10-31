c2164398e0d3cf39a579f961571cc184a75b9698
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-10-31 12:39:31 +0200

    chore: add lib build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -5,4 +6,19 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueJsx()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "lib/index.ts"),
+			name: "VueLocalScope",
+			fileName: "vue-local-scope",
+		},
+		rollupOptions: {
+			external: ["vue"],
+			output: {
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
 });
